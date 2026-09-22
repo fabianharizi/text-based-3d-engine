618b905019e43c7b659eadefd9ef7cbfb0cea88e
fabifabian <fabianharizi123@gmail.com>
2026-09-22 03:16:13 +0200

    Readed file from Object class

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
   public static void main(String[] args){
     World w = new World(32);
+    Object o = new Object();
+
+    o.loadFile("Cube.txt");
 
     w.renderCoordSys();
   }
--- Object.java
+++ Object.java
@@ -1,6 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Object {
   Face[] faces;
-  public Object(Face[] faces){
-    this.faces = faces;
+
+  public void loadFile(String pathname){
+    File obj = new File(pathname);
+
+    try (Scanner reader = new Scanner(obj)) {
+      while (reader.hasNextLine()) {
+        String data = reader.nextLine();
+        System.out.println(data);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
   }
 }
