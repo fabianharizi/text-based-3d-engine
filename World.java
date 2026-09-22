f2f170f0d5069a1586a31444a0b7de499dcd9629
fabifabian <fabianharizi123@gmail.com>
2026-09-22 04:17:29 +0200

    Defined each axis as matrix transformations in a 2D space

--- World.java
+++ World.java
@@ -1,17 +1,18 @@
 public class World {
   int size = 32;
   Object[] objects;
+  int[][] space;
+  int[][] x;
+  int[][] y;
+  int[][] z;
 
   public World(int size){
     this.size = size;
-  }
+    space = new int[size][size];
 
-  public void renderCoordSys(){
-    for(int i = 0; i < size; i++){
-      for(int j = 0; j < size; j++){
-        System.out.print("■ ");
-      }
-      System.out.println();
-    }
+    // {start coords}, {end coords}
+    x = new int[][] {{size-1, 0}, {size-1, size-1}};
+    y = new int[][] {{size-1, 0}, {0, 0}}; 
+    z = new int[][] {{size-1, 0}, {size/2, size/2}};
   }
 }
